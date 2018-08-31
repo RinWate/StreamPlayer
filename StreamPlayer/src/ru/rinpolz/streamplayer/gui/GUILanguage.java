be614ec379f178f5982b1df742e32c412986fdda
RinWate <RinWate@192.168.1.8>
2018-08-31 12:37:53 +0300

    Lang

--- StreamPlayer/src/ru/rinpolz/streamplayer/gui/GUILanguage.java
+++ StreamPlayer/src/ru/rinpolz/streamplayer/gui/GUILanguage.java
@@ -1,5 +1,17 @@
 package ru.rinpolz.streamplayer.gui;
 
-public class GUILanguage {
+import javax.swing.JFrame;
+
+public class GUILanguage extends JFrame {
+	
+	public GUILanguage(String title) {
+		this.setTitle(title);
+		this.setSize(300, 200);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 
 }
